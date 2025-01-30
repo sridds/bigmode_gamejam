178a02ea28cff2ae6453addbc54b6c5eef063bc8
zaxnathanson <zaxliyen@gmail.com>
2025-01-30 14:00:24 -0500

    Heal Every 10 combo

--- Bigmode2025/Assets/Scripts/PlayerHealthScript.cs
+++ Bigmode2025/Assets/Scripts/PlayerHealthScript.cs
@@ -1,10 +1,13 @@
 using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using static EnemyManager;
 
 public class PlayerHealthScript : MonoBehaviour
 {
     public float health = 100;
+    public float maxHealth = 100;
+
     [SerializeField] float invincibilityTime = 0.2f;
     float timer = 0;
     [SerializeField] Transform sprite;
@@ -18,10 +21,42 @@ public class PlayerHealthScript : MonoBehaviour
     public delegate void DamageTaken();
     public DamageTaken OnDamageTaken;
 
+    EnemyManager enemyManager;
 
+    [SerializeField] int comboBeforeHeal;
+    [SerializeField] int comboHealAmount;
+    [SerializeField] ParticleSystem healParticle;
+    bool hasHealed = false;
+    private void OnEnable()
+    {
+        enemyManager = FindFirstObjectByType<EnemyManager>();
+
+    }
     private void Update()
     {
         timer -= Time.deltaTime;
+        Heal();
+    }
+
+    public void Heal()
+    {
+        if (enemyManager.Combo % comboBeforeHeal == 0 && enemyManager.Combo != 0)
+        {
+            if (hasHealed == false)
+            {
+                Instantiate(healParticle, transform.position, Quaternion.identity, transform);
+                health += comboHealAmount;
+                if (health > maxHealth)
+                {
+                    health = maxHealth;
+                }
+                hasHealed = true;
+            }
+        }
+        else
+        {
+            hasHealed = false;
+        }
     }
 
     public void TakeDamage(float damage)
