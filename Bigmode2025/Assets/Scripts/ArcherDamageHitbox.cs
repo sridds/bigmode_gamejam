371c78d47de5c464baf9e06bc5760dc930ff9904
Konrad Sieniatecki-Smith <konradsmith99@gmail.com>
2025-01-31 14:50:03 -0500

    basic archers in

---
+++ Bigmode2025/Assets/Scripts/ArcherDamageHitbox.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ArcherDamageHitbox : MonoBehaviour
+{
+    [SerializeField] float damageToDeal = 20;
+    [SerializeField] EnemyHealthScript healthScript;
+    [SerializeField] ArcherEnemyAI archerEnemyAIScript;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //healthScript.InvincibilityFrames(0.01f);
+            archerEnemyAIScript.StopLunge();
+            collision.gameObject.GetComponent<PlayerHealthScript>().TakeDamage(damageToDeal);
+        }
+    }
+}
---
+++ Bigmode2025/Assets/Scripts/ArcherEnemyAI.cs
@@ -0,0 +1,331 @@
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ArcherEnemyAI : MonoBehaviour
+{
+    GameObject player;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] GameObject spriteObject;
+    [SerializeField] GameObject damageHitbox;
+    [SerializeField] GameObject shakeHolder;
+    [SerializeField] GameObject arrow;
+    [SerializeField] GhostTrail trail;
+
+    Rigidbody2D rb;
+
+    [Header("Attacking and Movement")]
+    [SerializeField] float targetStandDistance = 5;
+    [SerializeField] float targetAvoidDistance = 10;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float fireDistance = 8;
+    [SerializeField] float fireWaitTime = 0.5f;
+    [SerializeField] float arrowTravelTime = 0.3f;
+    [SerializeField] float fireCooldown = 1f;
+    [SerializeField] float standStillAfterFireTime = 0.2f;
+    [SerializeField] Transform arrowReturnPosition;
+
+    //If the player is right at the edge of the target distance, this prevents weird stuttering movement
+    [SerializeField] float walkingCooldown = 0.1f;
+    float timer = 0.1f;
+
+    public bool lunging = false;
+    public bool canLunge = true;
+    public bool isTerrified;
+
+    EnemyHealthScript healthScript;
+
+    [Header("Faces")]
+    [SerializeField]
+    private Sprite _neutralFace;
+
+    [SerializeField]
+    private Sprite _shockedFace;
+
+    [SerializeField]
+    private Sprite _preChargeFace;
+
+    [SerializeField]
+    private Sprite _chargeFace;
+
+    [SerializeField]
+    private Sprite _hurtFace;
+
+    [SerializeField]
+    private Sprite _decapitatedFace;
+
+    [SerializeField]
+    private SpriteRenderer _renderer;
+
+    [SerializeField]
+    private Animator _animator;
+
+    [Header("Audio")]
+    [SerializeField]
+    private AudioClip _lungeSound;
+
+    float terrifiedTimer;
+    bool canBeTerrified = true;
+    public bool knockedBack = false;
+    private AudioSource source;
+    bool chargingLunge = false;
+    EnemyManager enemyManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        healthScript = GetComponent<EnemyHealthScript>();
+
+        source = GetComponent<AudioSource>();
+
+        enemyManager = GameObject.Find("EnemyManager").GetComponent<EnemyManager>();
+        enemyManager.enemies.Add(gameObject);
+        enemyManager.UpdateCount();
+    }
+
+    public bool CanApplyKnockback()
+    {
+        if (lunging) return false;
+
+        return true;
+    }
+
+    public void ApplyKnockback()
+    {
+        Debug.Log("Ending Lunge");
+        StopAllCoroutines();
+        StartCoroutine(LungeEnding());
+
+        knockedBack = true;
+        rb.linearDamping = 3.0f;
+        _animator.SetBool("Damaged", true);
+        _animator.SetBool("Scared", false);
+        chargingLunge = false;
+
+        _renderer.sprite = _hurtFace;
+        isTerrified = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) < 4.0f && canBeTerrified && !knockedBack)
+        {
+            isTerrified = true;
+            terrifiedTimer = 0.0f;
+        }
+
+        if (isTerrified)
+        {
+            TerrifiedState();
+            return;
+        }
+
+        if (!lunging && !knockedBack && !chargingLunge)
+        {
+            MoveTowardsPlayer();
+
+            _animator.SetBool("Walking", true);
+            _renderer.sprite = _neutralFace;
+
+            if (player.transform.position.x > transform.position.x)
+            {
+                if (!_renderer.flipX)
+                {
+                    _renderer.flipX = true;
+
+                    arrow.transform.DOLocalMoveX(1.2f, 0.5f).SetEase(Ease.OutQuad);
+                }
+            }
+            else
+            {
+                if (_renderer.flipX)
+                {
+                    _renderer.flipX = false;
+
+                    arrow.transform.DOLocalMoveX(-1.2f, 0.5f).SetEase(Ease.OutQuad);
+                }
+            }
+        }
+
+        if (knockedBack && rb.linearVelocity.magnitude <= 1.0f)
+        {
+            knockedBack = false;
+            rb.linearDamping = 0.0f;
+            _animator.SetBool("Damaged", false);
+        }
+    }
+
+    void TerrifiedState()
+    {
+        rb.linearVelocity = Vector2.zero;
+        terrifiedTimer += Time.deltaTime;
+        _animator.SetBool("Walking", false);
+        _animator.SetBool("Scared", true);
+        _renderer.sprite = _shockedFace;
+
+        if (terrifiedTimer >= 0.5f)
+        {
+            CancelTerrifiedState();
+        }
+    }
+
+    void CancelTerrifiedState()
+    {
+        isTerrified = false;
+        _animator.SetBool("Scared", false);
+        _renderer.sprite = _neutralFace;
+        terrifiedTimer = 0.0f;
+    }
+
+    void MoveTowardsPlayer()
+    {
+        if (knockedBack) return;
+             
+        timer = timer - Time.deltaTime;
+
+        if (Vector3.Distance(player.transform.position, transform.position) > targetStandDistance)
+        {
+            canLunge = false;
+            if (timer <= 0)
+            {
+                float moveHorizontal = player.transform.position.x - transform.position.x;
+                float moveVertical = player.transform.position.y - transform.position.y;
+
+                rb.linearVelocity = new Vector3(moveHorizontal, moveVertical, 0).normalized * moveSpeed;
+            }
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) < targetAvoidDistance)
+        {
+            canLunge = false;
+            if (timer <= 0)
+            {
+                float moveHorizontal = player.transform.position.x - transform.position.x;
+                float moveVertical = player.transform.position.y - transform.position.y;
+
+                rb.linearVelocity = new Vector3(-moveHorizontal, -moveVertical, 0).normalized * moveSpeed;
+            }
+        }
+        else
+        {
+            canLunge = true;
+            rb.linearVelocity = Vector3.zero;
+            timer = walkingCooldown;
+
+        }
+    }
+
+    public IEnumerator LungeAttack()
+    {
+        chargingLunge = true;
+        rb.linearVelocity = Vector3.zero;
+
+        canBeTerrified = false;
+        CancelTerrifiedState();
+        _renderer.sprite = _preChargeFace;
+        //gearing up for lunge sprite change
+        spriteRenderer.color = Color.yellow;
+        canLunge = false;
+
+        
+        Vector3 playerVelocity = player.GetComponent<Rigidbody2D>().linearVelocity * (fireWaitTime + arrowTravelTime);
+        Vector3 predictedPosition = (player.transform.position);
+        Vector3 targetPosition = ((predictedPosition - transform.position).normalized * fireDistance) + transform.position;
+
+        //shakeHolder.transform.DOShakePosition(0.4f, new Vector3(0.5f, 0.0f), 25, 90, false, true, ShakeRandomnessMode.Full);
+
+        
+        // calculate spear rotation
+        var dir = (targetPosition - transform.position).normalized;
+        Quaternion lookRot = Quaternion.LookRotation(Vector3.forward, dir);
+        arrow.transform.DOLocalRotate(new Vector3(lookRot.eulerAngles.x, lookRot.eulerAngles.y, lookRot.eulerAngles.z + 360.0f), fireWaitTime * 0.75f, RotateMode.FastBeyond360);
+        
+
+        yield return new WaitForSeconds(fireWaitTime * 0.75f);
+        targetPosition = ((player.transform.position - transform.position).normalized * fireDistance) + transform.position;
+        yield return new WaitForSeconds(fireWaitTime * 0.25f);
+
+        dir = (targetPosition - transform.position).normalized;
+        lookRot = Quaternion.LookRotation(Vector3.forward, dir);
+        arrow.transform.DOLocalRotate(new Vector3(lookRot.eulerAngles.x, lookRot.eulerAngles.y, lookRot.eulerAngles.z), fireWaitTime / 2, RotateMode.Fast);
+
+
+        source.pitch = Random.Range(0.95f, 1.1f);
+
+        source.PlayOneShot(_lungeSound);
+
+        lunging = true;
+        chargingLunge = false;
+
+        _renderer.sprite = _chargeFace;
+        damageHitbox.SetActive(true);
+
+        //while lunging sprite change
+        spriteRenderer.color = Color.red;
+
+        //healthScript.InvincibilityFrames(arrowTravelTime);
+
+        //Lunge lerp
+        float elapsed = 0.0f;
+
+        Vector3 startPos = arrow.transform.position;
+
+        //trail.SetGhostTrailEnabled(true);
+
+        while (elapsed < arrowTravelTime)
+        {
+            arrow.transform.position = Vector3.Lerp(startPos, targetPosition, elapsed / arrowTravelTime);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        //waiting before enemy can lunge again
+        StartCoroutine(LungeEnding());
+
+        yield return null;
+    }
+
+    IEnumerator LungeEnding()
+    {
+        //Stagger after lunge
+        spriteRenderer.color = Color.green;
+
+        
+        damageHitbox.SetActive(false);
+        arrow.transform.position = arrowReturnPosition.position;
+
+        //trail.SetGhostTrailEnabled(false);
+        yield return new WaitForSeconds(standStillAfterFireTime);
+
+        arrow.transform.DOLocalRotateQuaternion(Quaternion.identity, 0.5f);
+        lunging = false;
+        canBeTerrified = true;
+
+        //waiting before enemy can lunge again
+        yield return new WaitForSeconds(fireCooldown);
+
+        canLunge = true;
+
+        //back to normal
+        spriteRenderer.color = Color.white;
+        yield return null;
+    }
+
+    public void StopLunge()
+    {
+        StopAllCoroutines();
+        lunging = false;
+
+        StartCoroutine(LungeEnding());
+
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
